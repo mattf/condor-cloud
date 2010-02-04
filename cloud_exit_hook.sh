5e1f302f673ae8f500024c4b7b97affb61a6d382
Matthew Farrellee <matt@redhat.com>
2010-02-03 22:29:38 -0500

    Introduced the cloud_exit_hook.sh to cleanup qcow2 disk images when VMs exit

---
+++ cloud_exit_hook.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+source /opt/condor-cloud/functions
+
+while read line; do
+   name="${line%% =*}"
+   value="${line#*= }"
+   case $name in
+     VM_XML ) VM_XML="$line" ;;
+   esac
+done
+
+DISK=$(echo $VM_XML | sed "s:.*<source file='\([^']*\)'/>.*:\1:")
+
+rm -f $DISK
+
+exit 0
