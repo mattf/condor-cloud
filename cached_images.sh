0b11943a203aff90961a594e2846e00f645c7dc5
Matthew Farrellee <matt@redhat.com>
2010-08-10 20:01:23 -0400

    Fixed cached_images.sh to return nothing instead of *, when no images cached

--- cached_images.sh
+++ cached_images.sh
@@ -3,6 +3,6 @@
 source /opt/condor-cloud/functions
 
 cd $CACHE
-echo "CACHED_IMAGES=\"$(echo * | tr ' ' '\n' | grep -v .qcow2 | tr '\n' ',')\""
+echo "CACHED_IMAGES=\"$(echo * | tr ' ' '\n' | grep -v -e '*' -e .qcow2 | tr '\n' ',')\""
 
 exit 0
